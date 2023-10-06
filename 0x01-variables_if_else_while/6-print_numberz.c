edef0056099515359bdcbe550d41ea2e5c822c9b
OK-Emmanuel <olawuniemmanuelk@gmail.com>
2023-10-06 16:03:10 -0700

    Retyped for compliance sake

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -15,3 +15,21 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
